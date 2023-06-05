88cf8222934dd952369cd87e12ffd709751cac8c
Ashir Afridi <ashirchannel765@gmail.com>
2023-06-05 20:20:28 +0500

    Ashir

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
-import { useState } from 'react';
+import React, { useEffect } from "react";
 import './App.css';
-const App = () =>{
-  const [counter,setCounter] = useState(0);
-  return (
-    <div className="App">
-    <button onClick={ ()=> setCounter(counter-1)}>-</button>
-    <h1>{counter}</h1>
-    <button onClick={ ()=> setCounter(counter+1)}>+</button>
-    </div>
-  );
+import Card from "./Components/Card";
+
+//f2ad7848
+
+const API_URL= "https://www.omdbapi.com/?apikey=f2ad7848";
+
+
+const movie1={
+    "Title": "Main Tera Hero",
+    "Year": "2014",
+    "imdbID": "tt2762334",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BZjBiYWU0ZmMtZDE0NC00ZTUxLWE3OTQtY2YyNjVjMTRlOGJjXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg"
+};
+const App = ()=>{
+
+    const searchMovies= async ($title)=>{
+        const response = await fetch(`${API_URL}&s=${$title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    };
+
+
+    useEffect(()=>{
+        searchMovies("Main Tera");
+    },[]);
+    return (
+        <div className="app">
+        <h1>Movie Land</h1>
+        <div className="search">
+            <input 
+                placeholder="Search"
+                value="Superman"
+                onChange={()=>{}}
+            />
+            {/* <img src={} /> */}
+        </div>
+        <div className="container">
+        <Card movie1={movie1}/>
+        <Card movie1={movie1}/>
+        <Card movie1={movie1}/>
+        <Card movie1={movie1}/>
+        <Card movie1={movie1}/>
+        </div>
+        </div>
+    );
 }
 
 export default App;
